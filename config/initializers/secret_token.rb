aea2e036859e83a863685caa399ec127ecc380d6
Chris Lerum <chris.lerum@gmail.com>
2014-04-05 11:19:49 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThemeParkReviews::Application.config.secret_key_base = '589ed9956b69df6c80254ea5cc2e3c92be1bab8ca17d75c3c1e0671b38522bca443ac295e686e0b70205dafc551551fd7f2cf4bb216522335df2e712b1b2447c'
